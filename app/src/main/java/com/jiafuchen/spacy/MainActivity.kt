7a141c24118b321881611e8f00504b7b3491fa75
Jiafu Chen <cjf110120130@gmail.com>
2023-05-08 20:04:19 +0200

    logo

--- app/src/main/java/com/jiafuchen/spacy/MainActivity.kt
+++ app/src/main/java/com/jiafuchen/spacy/MainActivity.kt
@@ -2,10 +2,13 @@ package com.jiafuchen.spacy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }
